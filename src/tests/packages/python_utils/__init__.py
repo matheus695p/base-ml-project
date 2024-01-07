04d3ce9c07fdf13493f06f2c552d3849a816cae4
matheus695p <Matheus_Pinto@mckinsey.com>
2024-01-07 17:50:17 -0300

    [feat]: ci with validations

--- src/tests/packages/python_utils/__init__.py
+++ src/tests/packages/python_utils/__init__.py
@@ -2,7 +2,3 @@
 
 Definitions to use for type hints.
 """
-
-from .typing import Matrix, Tensor, Vector
-
-__all__ = ["Vector", "Matrix", "Tensor"]
--- src/tests/packages/python_utils/typing/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Typing package.
-
-Definitions to use for type hints.
-"""
-
-from .test_tensors import Matrix, Tensor, Vector
-
-__all__ = ["Vector", "Matrix", "Tensor"]
