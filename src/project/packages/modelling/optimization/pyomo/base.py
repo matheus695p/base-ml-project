588d16d79a0142a93fb5cd8f67b439bbc4693926
matheus695p <Matheus_Pinto@mckinsey.com>
2024-04-22 14:35:58 -0500

    [fix]: some changes on readmes

---
+++ src/project/packages/modelling/optimization/pyomo/base.py
@@ -0,0 +1,9 @@
+# import logging
+# from abc import ABC
+# from typing import Dict, Union
+
+# import pandas as pd
+# from pyomo.core import ConcreteModel, Constraint, Param, Set, Var, Model
+# from pyomo.core.expr.numeric_expr import LinearExpression, NumericExpression
+# from pyomo.environ import NonNegativeReals, Objective, SolverFactory
+# from pyomo.util.infeasible import log_infeasible_constraints
